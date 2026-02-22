4a759e3b621d6a2365c242a7a8cccfe692c8600a
atharva shrestha <atharvakop7@gmail.com>
2026-02-22 13:45:02 +0545

    Checkout Page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,17 @@
+import {Routes, Route} from 'react-router'
 import HomePage from './Pages/HomePage'
 import './App.css'
+import CheckoutPage from './Pages/CheckoutPage'
 
 function App() {
 
   return (
-    <HomePage />
+    <>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
+    </>
   )
 }
 
